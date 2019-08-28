c378a3a2ba69c52c66e4abef67c1af3d8e6ab56c
BALAJI POTHULA <balan.pothula@gmail.com>
2019-08-28 15:09:56 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,11 +1,25 @@
+#!/bin/bash
 
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 12 August 2019,
+# Description : Installing / Compiling LuaRocks from Source Code.
 
+# update package repositories.
+sudo yum -y update
+
+# prerequisite - only once.
+# installing iptables.
 sudo yum install -y iptables
+# enabling ip_tables kernel module.
 sudo modprobe ip_tables
 
+# downloading docker rootless shell script and running.
 curl -sSL https://get.docker.com/rootless | sh
 
+# setting docker bin path.
 export PATH=$PATH:/home/ec2-user/bin
+# setting docker host.
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
 
+# starting docker with out root privilage.
 systemctl --user start docker
